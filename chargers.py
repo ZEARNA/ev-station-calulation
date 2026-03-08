6db1c1061e36b104c93d8abefd006c36a9128e47
ZEARNA <107101858+ZEARNA@users.noreply.github.com>
2026-03-08 17:42:35 +0700

    Add function to list chargers from database

--- chargers.py
+++ chargers.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from database import SessionLocal, ChargerDB
 
+
+def list_chargers():
+
+    db = SessionLocal()
+
+    chargers = db.query(ChargerDB).all()
+
+    db.close()
+
+    return chargers
