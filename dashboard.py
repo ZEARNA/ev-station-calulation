31b0aaca6ad4446cb28b4177b874e32fc7bf8de7
ZEARNA <107101858+ZEARNA@users.noreply.github.com>
2026-03-08 18:22:03 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from chargers import charger_data_page
-from calculator import calculate_project
 from database import SessionLocal, ChargerDB
+from calculator import calculate_project
 
 
 def user_dashboard():
@@ -12,10 +11,6 @@ def user_dashboard():
 
     chargers = db.query(ChargerDB).all()
 
-    if len(chargers) == 0:
-        st.warning("No charger models created")
-        return
-
     names = [c.name for c in chargers]
 
     selected = st.selectbox("Charger", names)
@@ -24,7 +19,7 @@ def user_dashboard():
 
     qty = st.number_input("Number of Chargers", value=2)
 
-    utilization = st.slider("Utilization %", 0, 100, 20) / 100
+    utilization = st.slider("Utilization %", 0, 100, 20)/100
 
     charge_price = st.number_input("Charging Price", value=8.0)
 
@@ -32,16 +27,17 @@ def user_dashboard():
 
     if st.button("Calculate ROI"):
 
-        cost, profit, roi = calculate_project(
+        cost,profit,roi = calculate_project(
             charger.power_kw,
             charger.price,
             qty,
             utilization,
             charge_price,
-            electricity_cost,
+            electricity_cost
         )
 
         st.metric("Project Cost", f"{cost:,.0f} THB")
+
         st.metric("Annual Profit", f"{profit:,.0f} THB")
 
         if roi:
