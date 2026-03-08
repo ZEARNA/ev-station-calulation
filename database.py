0dcac942ec8960219142e532a330c833ee530ed3
ZEARNA <107101858+ZEARNA@users.noreply.github.com>
2026-03-08 18:47:39 +0700

    Update database.py

--- database.py
+++ database.py
@@ -3,34 +3,39 @@ from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 DATABASE_URL = "sqlite:///ev_saas.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
 
 
-class UserDB(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String)
-    password = Column(String)
-    role = Column(String)
-
-
 class ChargerDB(Base):
+
     __tablename__ = "chargers"
 
     id = Column(Integer, primary_key=True)
+
     name = Column(String)
     type = Column(String)
 
     power_kw = Column(Float)
+
     price = Column(Float)
 
     max_connectors = Column(Integer)
 
+    dispensers = relationship(
+        "DispenserDB",
+        back_populates="charger"
+    )
+
 
 class DispenserDB(Base):
+
     __tablename__ = "dispensers"
 
     id = Column(Integer, primary_key=True)
@@ -38,11 +43,17 @@ class DispenserDB(Base):
     charger_id = Column(Integer, ForeignKey("chargers.id"))
 
     type = Column(String)
+
     connectors = Column(Integer)
+
     amp_per_connector = Column(Float)
 
-    charger = relationship("ChargerDB", back_populates="dispensers")
+    charger = relationship(
+        "ChargerDB",
+        back_populates="dispensers"
+    )
 
 
 def init_db():
+
     Base.metadata.create_all(engine)
