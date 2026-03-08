df9ee4b83bc81b294df6b2158b23b40083c3fd22
ZEARNA <107101858+ZEARNA@users.noreply.github.com>
2026-03-08 17:47:19 +0700

    Update database.py

--- database.py
+++ database.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 DATABASE_URL = "sqlite:///ev_saas.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
 
 
